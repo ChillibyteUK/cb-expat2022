20718360716eb5dfaf6130722d59694ffd500447
Matthew Pont <matthew.pont@gmail.com>
2026-06-04 13:36:32 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -358,7 +358,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_add_inline_script('datatables-js', "
     jQuery(document).ready(function($) {
 
-        var table = $('var table = $('#retire-abroad-table');')
+        var table = $('var table = $('#retire-abroad-table table');')
             .filter(function() {
                 return $(this).text().indexOf('The top 20 countries to retire abroad') === -1 &&
                        $(this).text().indexOf('Philippines') !== -1 &&
